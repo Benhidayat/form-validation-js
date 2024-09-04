78bd1e5df0b49a59dae45680806af158e2ccbb62
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2024-09-04 21:10:25 +0700

    created and styled sign up form

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Form Validation',
+            template: './src/index.html',
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    }
+}
